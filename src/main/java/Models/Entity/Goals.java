c3709dc2cd7b0f372cd4773284777b1e297ea924
Debois Francois <francoisdebois@gmail.com>
2023-03-29 15:24:27 +0200

    Custodian Day1:15:30

---
+++ src/main/java/Models/Entity/Goals.java
@@ -0,0 +1,30 @@
+package Models.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter @Setter
+public class Goals {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "goal_id", nullable = false)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column (name= "accomplished")
+    private boolean accomplished;
+    @Column(nullable = false)
+    private LocalDate date;
+    @Column(nullable = false)
+    private LocalTime beginTime;
+    @Column(nullable = false)
+    private LocalTime endTime;
+
+
+
+}
